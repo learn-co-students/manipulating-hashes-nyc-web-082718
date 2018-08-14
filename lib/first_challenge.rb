a3bd5378a739300a723bdc07fb417a324e097501
Michael Schwartz <michaelschwartz@Michaels-MBP-4.home>
2018-08-14 19:10:27 -0400

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,6 +15,17 @@ def first_challenge
 
   #your code here
 
+  contacts.each do |person, data|
+
+    data.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+         if value.include?("strawberry")
+            value.delete("strawberry")
+          end
+      end
+    end
+  end
+
 
   #remember to return your newly altered contacts hash!
   contacts
