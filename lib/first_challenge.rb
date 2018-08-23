0f7d156b09aa12bf4e4a0182278af1a2d38152eb
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-23 05:52:00 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,11 @@ def first_challenge
   }
 
   #your code here
-
+  contacts.each do |name, info|
+    if info[:favorite_icecream_flavors].include? "strawberry"
+      info[:favorite_icecream_flavors].delete("strawberry")
+    end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
